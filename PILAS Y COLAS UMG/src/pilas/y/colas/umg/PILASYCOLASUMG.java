892d35fb4b370b79d1c666581c63f6e6a23569c0
Gafesco <110783476+Gafesco@users.noreply.github.com>
2023-02-27 22:42:33 -0600

    Add files via upload

---
+++ PILAS Y COLAS UMG/src/pilas/y/colas/umg/PILASYCOLASUMG.java
@@ -0,0 +1,15 @@
+
+package pilas.y.colas.umg;
+
+/**
+ *
+ * @author Gabriel Escobar
+ */
+public class PILASYCOLASUMG {
+
+    public static void main(String[] args) {
+pilaycola frm = new pilaycola();
+frm.show();
+    }
+    
+}
